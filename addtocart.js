76d870cadd9a0d6e6c6780ec8e0c5ec096c1e1d2
KAILASANATHAN V <138500432+kailash0102@users.noreply.github.com>
2023-07-05 16:33:45 +0530

    Add files via upload

---
+++ addtocart.js
@@ -0,0 +1,21 @@
+.container{
+  display:flex;
+  width: 70%;
+  margin-bottom: 30px;
+
+}
+#root{
+  width: 60%;
+  display: grid;
+  grid-template-columns: repeat(2,1fr);
+  grid-gap: 20px;
+}
+.sidebar{
+  width: 40%;
+  border-radius: 5px;
+  background-color: white;
+  margin-left: 20px;
+  padding: 15px;
+  text-align: :center;
+  
+}
